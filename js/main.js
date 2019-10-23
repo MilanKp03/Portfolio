203a777211b7463635eae1a52e4eb9faceb38e21
MilanKp03 <40432786+MilanKp03@users.noreply.github.com>
2019-10-23 21:26:25 +0200

    2.0 section project's complete

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var myWindow;
+
+function myFunction() {
+   var myWindow = window.open("project1.html", "", "width=450,height=400,left=325,top=325");
+   
+ }
+             
+ function closeWin() {
+   myWindow.close();   // Closes the new window
+ }
